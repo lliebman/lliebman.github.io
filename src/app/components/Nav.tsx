177052c333f77ec084ad56fd0c3d4ab1b71b0bec
lillianliebman <lillian.liebman@indicative.com>
2023-02-01 14:28:31 +0200

    wip on nav

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -4,5 +4,44 @@ export {Nav}
 
 function Nav() {
   return (
-    <h1>Hi I'm the nav</h1>)
+    <nav>
+      {leftside()}
+      {rightside()}
+    </nav>)
+}
+
+function leftside() {
+  return (
+    <span>
+
+    </span>
+  )
+}
+
+function rightside() {
+  return (
+    <span>
+      <a>
+      {/*  linkedin*/}
+      <img src={linkedin} alt="linkedin" height="40px" />
+    </a>
+
+      <a
+        href="https://github.com/lliebman"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={github} alt="github" height="40px" />
+      </a>
+
+      {/*reume*/}
+
+      <a
+        href="mailto: lillian.liebman@gmail.com?subject="
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+      {/*  email pic*/}
+      </a></span>
+  )
 }
